517f8dba9f68b383e876f7122bee19c53271d58d
Vicente Arevalo <varevalo@uma.es>
2025-03-25 13:28:55 +0100

    Rewritten to show a figure with results.

--- matlab/test_gof_functions.m
+++ matlab/test_gof_functions.m
@@ -1,84 +1,110 @@
 % This script checks the behaviour of GOF functions with synthetic data.
 
+clear all
+close all
+
+dists = { create_model('EXP2', 2, @ExponentialRnd, @ExponentialGof)
+          create_model('LN3', 3, @LognormalRnd, @LognormalGof)
+          create_model('LL3', 3, @LoglogisticRnd, @LoglogisticGoF) };
+
 sizes = [10, 50, 100, 500, 1000, 5000];
 
-exp2.name = 'EXP2';
-exp2.nparams = 2;
-exp2.rnd = @ExponentialRnd;
-exp2.gof = @ExponentialGof;
+nsizes = length(sizes);
+ndists = length(dists);
+ntests = 100;
 
-ln3.name = 'LN3';
-ln3.nparams = 3;
-ln3.rnd = @LognormalRnd;
-ln3.gof = @LognormalGof;
+fig = figure;
 
-ll3.name = 'LL3';
-ll3.nparams = 3;
-ll3.rnd = @LoglogisticRnd;
-ll3.gof = @LoglogisticGoF;
+for s = 1:nsizes
+    sz = sizes(s);
 
-dists = {exp2, ln3, ll3};
+    % buffers
+    data = zeros(ndists,2);
+    data2 = zeros(ndists,2);
+    labels = cell(1,ndists);
 
-ntests = 500;
-for d = 1:length(dists)
+    for d = 1:ndists
+        dist = dists{d};
+        labels{d} = dist.name;
 
-    dist = dists{d};
-    for s = sizes
+        % filename = sprintf('./tests/%s_sz%d_true.txt', dist.name, sz);
+        % f = fopen(filename, 'w');
+        % fprintf(f, '%% p1(1)\t\tp1(2)\t\tp1(3)\t\tp2(1)\t\tp2(2)\t\tp2(3)\t\treject\t\tstat\t\tthresh\n');
+        % 
+        % filename = sprintf('./tests/%s_sz%d_false.txt', dist.name, sz);
+        % f2 = fopen(filename, 'w');
+        % fprintf(f2, '%% p1(1)\t\tp1(2)\t\tp1(3)\t\tp2(1)\t\tp2(2)\t\tp2(3)\t\treject\t\tstat\t\tthresh\n');
 
-        filename = sprintf('./tests/%s_sz%d_true.txt', dist.name, s);
-        f = fopen(filename, 'w');
-        fprintf(f, '%% p1(1)\t\tp1(2)\t\tp1(3)\t\tp2(1)\t\tp2(2)\t\tp2(3)\t\treject\t\tstat\t\tthresh\n');
+        for n = 1:ntests
+            params = rand(1, dist.nparams)*10; % params used to generate & check the samples
+            params2 = rand(1, dist.nparams)*10; % params used to calculate Gof (false)
 
-        filename = sprintf('./tests/%s_sz%d_false.txt', dist.name, s);
-        f2 = fopen(filename, 'w');
-        fprintf(f2, '%% p1(1)\t\tp1(2)\t\tp1(3)\t\tp2(1)\t\tp2(2)\t\tp2(3)\t\treject\t\tstat\t\tthresh\n');
+            if dist.nparams == 2
+                samples = dist.rnd(params(1), params(2), 1, sz);
+                [reject1, stat1, thresh1] = dist.gof(samples, params(1), params(2));
+                [reject2, stat2, thresh2] = dist.gof(samples, params2(1), params2(2));
 
-        if dist.nparams == 2
+            elseif dist.nparams == 3
+                samples = dist.rnd(params(1), params(2), params(3), 1, sz);
+                [reject1, stat1, thresh1] = dist.gof(samples, params(1), params(2), params(3));
+                [reject2, stat2, thresh2] = dist.gof(samples, params2(1), params2(2), params2(3));
 
-            for n = 1:ntests
-                params = rand(1, dist.nparams)*10; % params used to generate & check the samples
-                samples = dist.rnd(params(1), params(2), 1, s);
-                [reject, stat, thresh] = dist.gof(samples, params(1), params(2));
-                print_data(f, params, params, reject, stat, thresh);
-
-                params2 = rand(1, dist.nparams)*10; % params used to calculate Gof (false)
-                [reject, stat, thresh] = dist.gof(samples, params2(1), params2(2));
-                print_data(f2, params, params2, reject, stat, thresh);
+            else
+                error('number of params wrong!.');
             end
 
-        elseif dist.nparams == 3
+            % print_data(f, params, params, reject1, stat1, thresh1);
+            % print_data(f2, params, params2, reject2, stat2, thresh2);
 
-            for n = 1:ntests
-                params = rand(1, dist.nparams)*10; % params used to generate & check the samples
-                samples = dist.rnd(params(1), params(2), params(3), 1, s);
+            data(d,1) = data(d,1) + (1 - reject1)*100/ntests;
+            data(d,2) = data(d,2) + 100 - (1 - reject1)*100/ntests;
+            data2(d,1) = data2(d,1) + reject2*100/ntests;
+            data2(d,2) = data2(d,2) + 100 - reject2*100/ntests;
 
-                [reject, stat, thresh] = dist.gof(samples, params(1), params(2), params(3));
-                print_data(f, params, params, reject, stat, thresh);
-
-                params2 = rand(1, dist.nparams)*10; % params used to calculate Gof (false)
-                [reject, stat, thresh] = dist.gof(samples, params2(1), params2(2), params2(3));
-                print_data(f2, params, params2, reject, stat, thresh);
-            end
-
-        else
-            error('number of params wrong!.');
         end
 
-        fclose(f);
-        fclose(f2);
+        % fclose(f);
+        % fclose(f2);
     end
+
+    subplot(2,nsizes,s)
+    bar(data,'stacked');
+    subtitle(['GoF with true params (size=' num2str(sz) ')']);
+    xlabel('Distribution'); ylabel('GoF');
+    legend('Success=reject0', 'Failure=reject1');
+    xticklabels(labels);
+    ylim([0,100]);
+
+    subplot(2,nsizes,nsizes+s)
+    bar(data,'stacked');
+    subtitle(['GoF with false params (size=' num2str(sz) ')']);
+    xlabel('Distribution'); ylabel('GoF');
+    legend('Success=reject1', 'Failure=reject0');
+    xticklabels(labels);
+    ylim([0,100]);
+
+    % data
 end
 
-return
+return;
 
-function print_data(f, params, params2, reject, stat, thresh)
+% function print_data(f, params, params2, reject, stat, thresh)
+% 
+%     if numel(params) == 2, params(3) = 0; end
+%     if numel(params2) == 2, params2(3) = 0; end
+% 
+%     fprintf(f, '%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n', ...
+%         params(1),  params(2),  params(3), ...
+%         params2(1), params2(2), params2(3), ...
+%         reject, stat, thresh);
+% 
+% end
 
-    if numel(params) == 2, params(3) = 0; end
-    if numel(params2) == 2, params2(3) = 0; end
+function model = create_model(name, nparams, frnd, fgof)
 
-    fprintf(f, '%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n', ...
-        params(1),  params(2),  params(3), ...
-        params2(1), params2(2), params2(3), ...
-        reject, stat, thresh);
+    model.name = name;
+    model.nparams = nparams;
+    model.rnd = frnd;
+    model.gof = fgof;
 
 end
