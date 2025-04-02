661818e4f587e9d11e35005534f54d3146ce3e73
jafma <jafma@debian-BULLSEYE-live-builder-AMD64>
2025-04-02 10:14:43 +0200

    Returned to D'Agostino original gof for lognormal, since it had some bugs but now i know it should be equivalent to the one i got from Matlab repo

--- matlab/LognormalGof.m
+++ matlab/LognormalGof.m
@@ -1,7 +1,7 @@
 function [reject,stat,thresh,pvalue] = LognormalGof(x,offset,mu,sigma)
 
     n = length(x);
-    if (sigma <= 0) 
+    if (sigma <= 0)
         error('Invalid lognormal distr.');
     end
     if (min(x) < offset) % that model cannot assess these data
@@ -12,60 +12,64 @@ function [reject,stat,thresh,pvalue] = LognormalGof(x,offset,mu,sigma)
         return;
     end
 
-% Taken from https://es.mathworks.com/matlabcentral/fileexchange/60147-normality-test-package
-
-    y = sort(log((x(:).') - offset)); % transform from lognormal to normal with expectation MU and std SIGMA
-    ui = normcdf(y,mu,sigma); % if taking mu,sigma from the data:  ui=normcdf(zscore(y),0,1); % zscore embed the mean and sigma estimated from the data
-    oneminusui = sort(1-ui);
-    i = 1:n;
-    lastt = (2*i-1).*(log(ui)+log(oneminusui));
-    asquare = -n-(1/n)*sum(lastt);
-    adj = 1+0.75/n+2.25/(n^2);
-    AD = asquare*adj;
-     
-    if AD<=0.2
-        pvalue=1-exp(-13.436+101.14*AD-223.73*AD^2);
-    elseif AD<=0.34
-        pvalue=1-exp(-8.318+42.796*AD-59.938*AD^2);
-    elseif AD<=0.6
-        pvalue=exp(0.9177-4.279*AD-1.38*AD^2);
-    elseif AD<=153.467
-        pvalue=exp(1.2937*AD-5.709*AD+0.0186*AD^2);
-    else
-        pvalue=0;
-    end
-
-    stat = AD;
-    thresh = NaN;
-    if pvalue <= 0.05 % equivalently, the statistic is greater than the threshold
-        reject = 1; % the null hypothesis (data come from normal) should be rejected: the possible normality is caused by noise
-    else
-        reject = 0; % cannot reject the hypothesis of normality
-    end
+% % Taken from https://es.mathworks.com/matlabcentral/fileexchange/60147-normality-test-package
+% % Paper of 2017 in tyrell project /ejecucion/docs/A Compilation of Some Popular Goodness of Fit Tests for Normal Distribution.pdf
+% % They say they take it from D'Agostino p. 122 and table 4.9 in p.127
+% 
+%     y = sort(log((x(:).') - offset)); % transform from lognormal to normal with expectation MU and std SIGMA
+%     ui = normcdf(y,mu,sigma); % if taking mu,sigma from the data:  ui=normcdf(zscore(y),0,1); % zscore embed the mean and sigma estimated from the data
+%     oneminusui = sort(1-ui);
+%     i = 1:n;
+%     lastt = (2*i-1).*(log(ui)+log(oneminusui));
+%     asquare = -n-(1/n)*sum(lastt);
+%     adj = 1+0.75/n+2.25/(n^2);
+%     AD = asquare*adj;
+% 
+%     % this is from table 4.9 in D'Agostino and serves to calculate p-value, then compare it to the significance level
+%     if AD<=0.2
+%         pvalue=1-exp(-13.436+101.14*AD-223.73*AD^2);
+%     elseif AD<=0.34
+%         pvalue=1-exp(-8.318+42.796*AD-59.938*AD^2);
+%     elseif AD<=0.6
+%         pvalue=exp(0.9177-4.279*AD-1.38*AD^2);
+%     elseif AD<=153.467
+%         pvalue=exp(1.2937*AD-5.709*AD+0.0186*AD^2);
+%     else
+%         pvalue=0;
+%     end
+% 
+%     stat = AD;
+%     thresh = NaN;
+%     if pvalue <= 0.05 % equivalently, the statistic is greater than the threshold
+%         reject = 1; % the null hypothesis (data come from normal) should be rejected: the possible normality is caused by noise
+%     else
+%         reject = 0; % cannot reject the hypothesis of normality
+%     end
 
 % Based on D'Agostino p. 122: parameters unknown.
-%     xord = sort(x);     
-%     logxord = log(xord); % still ordered, now normal
-%     m = mean(x);
-%     w = (logxord - mu) / sigma;
-%     Z = normcdf(w,0,1);
-% 
-%     % calculate statistic: A2 for case 3 (both parameters were deduced from
-%     % the same sample). This statistic measures the squared distance
-%     % between experimental and theoretical Zs, and, indirectly, between 
-%     % theoretical and experimental Xs (p. 100)
-%     sumatoria = sum(([1:n]*2-1).*log(Z)+(2*n+1-2*[1:n]).*log(1-Z)); % page 101, bottom formula
-%     A2 = -n - (1/n)* sumatoria;
-%     % do the following since parameters come from sample (D'Agostino table 4.7)
-%     A2 = A2*(1 + 0.75/n + 2.25/n^2); 
-%     stat = A2;
-% 
-%     % test the hypothesis 
-%     thresh = 2.492; % table 4.2 D'Agostino
-%     if (stat > thresh) 
-%         reject=1; % reject
-%     else
-%         reject=0; % cannot reject
-%     end 
+
+    xord = sort(x - offset); % go to a non-shifted sample from a non-shifted lognormal
+    logxord = log(xord); % still ordered, now normal
+    m = mean(x);
+    w = (logxord - mu) / sigma;
+    Z = normcdf(w,0,1);
+
+    % calculate statistic: A2 for case 3 (both parameters were deduced from
+    % the same sample). This statistic measures the squared distance
+    % between experimental and theoretical Zs, and, indirectly, between 
+    % theoretical and experimental Xs (p. 100)
+    sumatoria = sum(([1:n]*2-1).*log(Z)+(2*n+1-2*[1:n]).*log(1-Z)); % page 101, bottom formula
+    A2 = -n - (1/n)* sumatoria;
+    % do the following since parameters come from sample (D'Agostino table 4.7)
+    A2 = A2*(1 + 0.75/n + 2.25/n^2); 
+    stat = A2;
+
+    % test the hypothesis 
+    thresh = 0.752; % according to table 4.7
+    if (stat > thresh) 
+        reject=1; % reject
+    else
+        reject=0; % cannot reject
+    end 
 
 end
