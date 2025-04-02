7939657ee876099163233afb399e384f2f8d3862
jafma <jafernandez@uma.es>
2025-04-02 17:52:22 +0200

    Included some sanity checks in the Gofs and provision for working with or without known parameters

--- matlab/LognormalGof.m
+++ matlab/LognormalGof.m
@@ -1,21 +1,34 @@
-function [reject,stat,thresh,pvalue] = LognormalGof(x,offset,mu,sigma)
+function [reject,stat,thresh] = LognormalGof(x,offset,mu,sigma,flagprevmodel)
 
-    n = length(x);
-    if (sigma <= 0)
-        error('Invalid lognormal distr.');
+    % consider by default that alpha and beta have been estimated from
+    % samples.
+    if nargin == 4
+        flagprevmodel = 0;
     end
-    if (min(x) < offset) % that model cannot assess these data
-        reject = 1;
-        stat = NaN;
-        thresh = NaN;
-        pvalue = 0;
-        return;
+
+    if (offset < 0) 
+        error('Invalid lognormal distr.: offset < 0');
     end
+    
+    if (sigma <= 0) 
+        error('Invalid lognormal distr.: sigma <= 0');
+    end
+
+    n = numel(x);
+    x = reshape(x,1,n); % force X is a row vector
 
 % % Taken from https://es.mathworks.com/matlabcentral/fileexchange/60147-normality-test-package
 % % Paper of 2017 in tyrell project /ejecucion/docs/A Compilation of Some Popular Goodness of Fit Tests for Normal Distribution.pdf
 % % They say they take it from D'Agostino p. 122 and table 4.9 in p.127
 % 
+%    if (min(x) < offset) % that model cannot assess these data
+%        reject = 1;
+%        stat = NaN;
+%        thresh = NaN;
+%        pvalue = 0;
+%        return;
+%    end
+%
 %     y = sort(log((x(:).') - offset)); % transform from lognormal to normal with expectation MU and std SIGMA
 %     ui = normcdf(y,mu,sigma); % if taking mu,sigma from the data:  ui=normcdf(zscore(y),0,1); % zscore embed the mean and sigma estimated from the data
 %     oneminusui = sort(1-ui);
@@ -46,9 +59,15 @@ function [reject,stat,thresh,pvalue] = LognormalGof(x,offset,mu,sigma)
 %         reject = 0; % cannot reject the hypothesis of normality
 %     end
 
-% Based on D'Agostino p. 122: parameters unknown.
+	% Based on D'Agostino p. 122: parameters unknown.
 
     xord = sort(x - offset); % go to a non-shifted sample from a non-shifted lognormal
+    if (xord(1) < 0) % that model cannot assess these data
+        reject = 1;
+        stat = Inf;
+        thresh = NaN;
+        return;
+    end
     logxord = log(xord); % still ordered, now normal
     m = mean(x);
     w = (logxord - mu) / sigma;
@@ -60,16 +79,23 @@ function [reject,stat,thresh,pvalue] = LognormalGof(x,offset,mu,sigma)
     % theoretical and experimental Xs (p. 100)
     sumatoria = sum(([1:n]*2-1).*log(Z)+(2*n+1-2*[1:n]).*log(1-Z)); % page 101, bottom formula
     A2 = -n - (1/n)* sumatoria;
-    % do the following since parameters come from sample (D'Agostino table 4.7)
-    A2 = A2*(1 + 0.75/n + 2.25/n^2); 
+    % do the following if parameters come from sample (D'Agostino table 4.7)
+    if ~flagprevmodel
+    	A2 = A2 * (1 + 0.75/n + 2.25/n^2);
+    end
     stat = A2;
-
-    % test the hypothesis 
-    thresh = 0.752; % according to table 4.7
+    
+    % --- hypothesis test
+    if flagprevmodel
+    	thresh = 2.492; % known parameters = previous model (n>=5)
+                        % table 4.2 D'Agostino
+    else % unknown parameters, estimated from the very sample
+        thresh = 0.752; % table 4.7, upper tail.
+    end
     if (stat > thresh) 
         reject=1; % reject
     else
         reject=0; % cannot reject
-    end 
-
+    end
+    
 end
