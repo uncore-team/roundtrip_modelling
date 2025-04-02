f6b6c51c11b0279ea61d3eb496fcd0061a23fee8
jafma <jafernandez@uma.es>
2025-04-02 18:01:33 +0200

    Changed the ExponentialFit algorithm for a simpler MLE that forces offset >= 0

--- matlab/ExponentialFit.m
+++ matlab/ExponentialFit.m
@@ -1,25 +1,30 @@
 function [alpha,beta] = ExponentialFit(x)
-% According to D'Agostino, p. 141 but corrected for the final beta to be
-% 1/mean (not correct in the book, where they say they estimate beta when 
-% actually they are estimating the mean).
-% Shifted exponential pdf in D'Agostino p. 133
-% 1/beta == mean of the distribution (no offset)
-% alpha == location of the distribution
+% Using a simple MLE estimation that forces alpha > 0
 
-% PROBLEM: This method produces negative alpha in a high proportion of
-%		   the estimations
- 
-     minlen = 10;
- 
-     n = length(x);
- 	if (n < minlen)
- 		error('Cannot fit anything with less than %d values',minlen);
- 	end
- 
-     mi = min(x);
-     mu = n * (mean(x) - mi) / (n - 1); % estimate of the (non-shifted) mean
-     alpha = mi - mu / n; % offset
- 
-     beta = 1/mu; % beta is the reciprocal of the mean (in the book they use beta as the mean)
+    alpha = min(x); % offset
+    beta = 1.0 / mean(x - alpha); % beta is the reciprocal of the mean (in the book they use beta as the mean)
+
+% % According to D'Agostino, p. 141 but corrected for the final beta to be
+% % 1/mean (not correct in the book, where they say they estimate beta when 
+% % actually they are estimating the mean).
+% % Shifted exponential pdf in D'Agostino p. 133
+% % 1/beta == mean of the distribution (no offset)
+% % alpha == location of the distribution
+% 
+% % PROBLEM: This method produces negative alpha in a high proportion of
+% the estimations
+% 
+%     minlen = 10;
+% 
+%     n = length(x);
+% 	if (n < minlen)
+% 		error('Cannot fit anything with less than %d values',minlen);
+% 	end
+% 
+%     mi = min(x);
+%     mu = n * (mean(x) - mi) / (n - 1); % estimate of the (non-shifted) mean
+%     alpha = mi - mu / n; % offset
+% 
+%     beta = 1/mu; % beta is the reciprocal of the mean (in the book they use beta as the mean)
 
 end
