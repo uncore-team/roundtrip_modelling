64e51faffc2991e33ca9f35e86861b4eab233f33
jafma <jafma@debian-BULLSEYE-live-builder-AMD64>
2025-04-22 12:49:59 +0200

    Calculated new thresholds for the GoFs through MonteCarlo and incorporated into the gof functions·

--- onlineransac/matlab/ExponentialGof.m
+++ onlineransac/matlab/ExponentialGof.m
@@ -48,17 +48,33 @@ function [reject,stat,thresh] = ExponentialGof(x,alpha,beta,modelnotfromdata)
     % ---- test the hypothesis 
     if modelnotfromdata
     	thresh = 2.492; % known parameters (n>=5)
-                        % table 4.2 D'Agostino
+                        % table 4.2 D'Agostino; confirmed with MonteCarlo
+                        % experiments (test_tabulategofthrs.m)
     else % parameters come from the very data
-        if n > 100
-    	    thresh = 1.321; % long samples
-        else
-    	    ns = [5,10,15,20,25,50,100]; % lengths listed in D'Agostino table 4.15
-    	    ts = [0.725,0.920,1.009,1.062,1.097,1.197,1.250];
-    	    thresh = spline(ns,ts,n); % best interpolation since points are non-linear
-    								    % do not include the point at > 100 since it is
-    								    % too far and distorts the spline strongly
-        end
+
+        % the following is D'Agostino but does not work for use since he
+        % assumes only one parameter unknown, but we have 2; thus we have
+        % conducted new MonteCarlo experiments to deduce the threshold in
+        % this case
+        %
+        % if n > 100
+    	%     thresh = 1.321; % long samples
+        % else
+    	%     ns = [5,10,15,20,25,50,100]; % lengths listed in D'Agostino table 4.15
+    	%     ts = [0.725,0.920,1.009,1.062,1.097,1.197,1.250];
+    	%     thresh = spline(ns,ts,n); % best interpolation since points are non-linear
+    			% 					    % do not include the point at > 100 since it is
+    			% 					    % too far and distorts the spline strongly
+        % end
+
+
+        % MonteCarlo results in test_tabulategofthrs.m; fitting at the end
+        % section of that script:
+
+        fcn1 = @(b,t) b(1).*exp(t.*b(2))+b(3).*exp(t.*b(4));
+        parms = [2.984038306464773;-0.040726018747384;1.584140326155763;-0.000317691760535];
+        thresh = fcn1(parms,n);
+
     end    
     if (stat > thresh) % equivalently, the p-value is smaller than the significant level
         reject=1; % reject
--- onlineransac/matlab/test_tabulategofthrs.m
+++ onlineransac/matlab/test_tabulategofthrs.m
@@ -9,7 +9,7 @@ clc;
 mtrue = ModelCreate('LL3');
 parmsunknown = 1; % 1 - tabulate for unknown parameters that are deduced from the sample; 0 - tabulate for the true params that generate the sample
 samplesizes = 20:10:500; % samples sizes to tabulate
-numtests = 10000; % monte carlo simulation on that number of samples
+numtests = 100000; % monte carlo simulation on that number of samples
 alpha = 0.05; % significance level to tabulate for
 
 % internal parms that should not be changed
@@ -18,10 +18,15 @@ usesimplerinterp = 1; % 1 == adjust better to the thresholds in D'Agostino for k
 
 measurements = cell(1,length(samplesizes));
 results = nan(1,length(samplesizes));
+if parmsunknown
+    nametrace = sprintf('%s (parms from sample)',mtrue.type);
+else
+    nametrace = sprintf('%s (parms true)',mtrue.type);
+end
 t0ext = tic;
 for f = 1:length(samplesizes)
     samplesize = samplesizes(f);
-    fprintf('TABULATING %s for size %d... ',mtrue.type,samplesize);
+    fprintf('TABULATING %s for size %d... ',nametrace,samplesize);
     toc(t0ext)
 
     stats = nan(1,numtests);
@@ -97,6 +102,10 @@ for f = 1:length(samplesizes)
             end
             fprintf('\t\tThreshold: %f with cdf-1 = %f and cdf = %f (mean %f)\n',...
                     results(f),cdfsofar,newcdfsofar,(cdfsofar + newcdfsofar)/2);
+            if traceinternal
+                fprintf('\t');
+                toc(t0)
+            end            
             break;
         end
         cdfsofar = newcdfsofar;
@@ -112,6 +121,8 @@ else
 end
 save(sprintf('%s.mat',namefi));
 
+%%
+
 figure;
 subplot(1,2,1);
 plot(samplesizes,results,'.');
@@ -120,3 +131,209 @@ xlabel('sample size');
 ylabel('threshold for gof');
 subplot(1,2,2);
 drawHisto(results,'histogram','threshold for gof');
+
+%%  --- tries to fit the EXP2-parms-from-sample results
+
+if ~strcmp(mtrue.type,'EXP2')
+    error('Cannot run this section for non-EXP2 distrib');
+end
+% NOTES:
+%   The curve tends to +Inf at 0 and has a more abrupt change in its
+%   decreasing ratio than a/(X^b), ending almost horizontal at around y =
+%   mean(results(29:end)) = 1.3966. For a size >= 300, this can be a
+%   suitable threshold.
+%   With the double exponential it fits ok in the Curve Fitter app:
+%       Startpoint: [1.625;-0.0188;1.4653;-0.0002] (calculated
+%       heuristically -"optimized"- by the app based on characteristics of
+%       the data such as multiple decay speeds, initial values, preliminary 
+%       one-exponential adjustment, derivatives, etc; actually Matlab does
+%       not specify how)
+
+% My code:
+
+x0 = [1.625;-0.0188;1.4653;-0.0002];
+fcn1 = @(b,t) b(1).*exp(t.*b(2))+b(3).*exp(t.*b(4));
+[parms, fval] = fminsearch (@ (b) norm(results - fcn1(b, samplesizes)), x0)
+plot(samplesizes,results,'.');
+grid;
+hold on;
+plot(samplesizes,fcn1(parms,samplesizes),'r.-');
+
+% The Matlab automatic fitting code (produces the same result):
+
+[xData, yData] = prepareCurveData( samplesizes, results );
+% Set up fittype and options.
+ft = fittype( 'exp2' );
+opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+opts.Display = 'Off';
+opts.StartPoint = [1.62051288611057 -0.0188232367797166 1.46532687005696 -0.000154533652141461];
+% Fit model to data.
+[fitresult, gof] = fit( xData, yData, ft, opts );
+% Plot fit with data.
+figure( 'Name', 'untitled fit 1' );
+h = plot( fitresult, xData, yData );
+legend( h, 'results vs. samplesizes', 'untitled fit 1', 'Location', 'NorthEast', 'Interpreter', 'none' );
+% Label axes
+xlabel( 'samplesizes', 'Interpreter', 'none' );
+ylabel( 'results', 'Interpreter', 'none' );
+grid on
+
+%% --- tries to fit the LN3-parms-from-sample results
+
+% NOTES:
+%   No need for fit. Uncorrelated threshold approximately = 1.178 with
+%   sigma = 0.186, i.e., 95% between 0.992 and 1.364
+
+drawHisto(results,'LN3-parms-from-sample','MonteCarlo threshold');
+mean(results)
+
+%% --- tries to fit the LL3-parms-from-sample results
+
+% NOTES:
+%   Follow something similar to a log curve, but estabilizes for x > 100, 
+%   with certain slight decay for x > 420.
+%   It seems reasonable to divide the curve into these parts:
+%       x < 120 -> log fit after some shift/scale; alternatively, fit a
+%                  first order response; alternatively, a 5th deg poly. 
+%                  Anyway, the final value should be around
+%                  6.238 to link with the next part.
+%       (a line interpolation between 110 and 120 to close the gap; visually,
+%        a linear interpolation between 90 sand 120 is more acceptable)
+%       x in [120,370] -> horizontal; mean value = 6.238, sigma = 0.120
+%       x > 370 -> linear (decreasing) fit with fixed intercept to join the
+%                  previous part at 6.238; i.e., y = ax+b where y = 6.238
+%                  if x = 370 => b = 6.238 - a * 370
+%                  This is done this way:
+%                       - substract 6.238 from the data to have an intercept
+%                       of 0 in the new data y'
+%                       - least-squares fit a curve y = mx to the new data by
+%                       this: m = sum(x .* y') / sum(x.^2);
+%                       - the resulting fit is y = mx + 6.238
+
+close all;
+clc;
+figure;
+grid;
+hold on;
+
+% second part
+
+inds2 = find((samplesizes >= 120) & (samplesizes <= 370));
+xs2 = samplesizes(inds2);
+ys2 = results(inds2); 
+horizfit = mean(ys2) % horizontal fit
+ys2fit = horizfit * ones(1,length(xs2));
+plot(xs2,ys2,'mo');
+plot(xs2,ys2fit,'r.-');
+
+% first part 
+
+inds1 = find(samplesizes < 120);
+xs1 = samplesizes(inds1) - samplesizes(1); % shift data to make x_0 = 0
+ys1 = results(inds1) - results(1); % shift data to make y(0) = 0
+fv = horizfit - results(1); % final value (forced) for the 1st order response
+indm = round(length(xs1)/2); % middle point taken to calc an initial guess
+xm = xs1(indm);
+ym = ys1(indm);
+bstart = -log(-ym/fv+1)/xm % fit deterministically a 1st order with that middle point and final value
+fcn1 = @(b,x) fv * b(1) * (1 - exp(-b(2) * x)); % forced to end at fv
+[parms, fval] = fminsearch (@ (b) norm(ys1 - fcn1(b,xs1)), [1;bstart]) % fit a better 1st order, but it will not end at fv exactly (it cannot be forced because fv is at x = Inf, not at x = inds1(end))
+
+plot(xs1 + samplesizes(1),ys1 + results(1),'o');
+grid; 
+hold on; 
+plot(xs1(end) + samplesizes(1),fv + results(1),'m*');
+plot(xs1 + samplesizes(1),fcn1(parms,xs1) + results(1),'r:.');
+
+% first part alternative - 5th deg poly
+%
+% f(x) = p1*x^5 + p2*x^4 + p3*x^3 + p4*x^2 + p5*x + p6
+% 
+% Coefficients and 95% Confidence Bounds
+%        Value      Lower     Upper  
+% p1    -0.0000    -0.0000    0.0000
+% p2    0.0000     -0.0000    0.0000
+% p3    -0.0000    -0.0002    0.0002
+% p4    -0.0015    -0.0139    0.0109
+% p5    0.1927     -0.1347    0.5201
+% p6    -0.7079    -3.8399    2.4242
+% 
+% Goodness of Fit
+%             Value  
+% SSE         0.0117
+% R-square    0.9992
+% DFE         4.0000
+% Adj R-sq    0.9982
+% RMSE        0.0541
+
+% p1 is x^5. It looks like it is rather a third or second order poly, but
+% actually quitting those coeffs it does not work; fitting lesser polys
+% works worst too
+parms = [-0.000000000421424;0.000000118010409;-0.000004990075374;-0.001491034210112;0.192700180267995;-0.707864114472733];
+xs1 = samplesizes(inds1);
+ys1fit5 = parms(1) * xs1.^5 + parms(2) * xs1.^4 + parms(3) * xs1.^3 + parms(4) * xs1.^2 + parms(5) * xs1 + parms(6);
+plot(xs1,ys1fit5,'r.-');
+
+% automatically generated code from Curve Fitting app:
+%
+% [xData, yData] = prepareCurveData( xs1, ys1 ); % xs1,ys1 without shifting
+% or scaling!
+% 
+% % Set up fittype and options.
+% ft = fittype( 'poly5' );
+% 
+% % Fit model to data.
+% [fitresult, gof] = fit( xData, yData, ft );
+% 
+% % Plot fit with data.
+% figure( 'Name', 'untitled fit 1' );
+% h = plot( fitresult, xData, yData );
+% legend( h, 'ys1 vs. xs1', 'untitled fit 1', 'Location', 'NorthEast', 'Interpreter', 'none' );
+% % Label axes
+% xlabel( 'xs1', 'Interpreter', 'none' );
+% ylabel( 'ys1', 'Interpreter', 'none' );
+% grid on
+
+
+% third part
+
+inds3 = find(samplesizes > 370);
+xs3 = samplesizes(inds3) - samplesizes(inds3(1));
+ys3 = results(inds3) - horizfit;
+m = sum(xs3 .* ys3) / sum(xs3.^2)
+plot(xs3 + samplesizes(inds3(1)),ys3 + horizfit,'co');
+plot(xs3 + samplesizes(inds3(1)),m * xs3 + horizfit,'r.-');
+
+% ALL PARTS TOGETHER - code to go to LoglogisticGoF.m
+
+xs = samplesizes(1):samplesizes(end);
+ys = [];
+for n = xs
+
+    if n < 90 % firs part: 5th poly
+
+        parms = [-0.000000000421424;0.000000118010409;-0.000004990075374;-0.001491034210112;0.192700180267995;-0.707864114472733];
+        thresh = parms(1) * n^5 + parms(2) * n^4 + parms(3) * n^3 + parms(4) * n^2 + parms(5) * n + parms(6);
+
+    elseif n < 120 % link between first and second part: linear interpolation
+
+        x1 = 90;
+        y1 = 6.174206416983618; % value of the first part at 90
+        x2 = 120;
+        y2 = 6.237947960378145; % value of the second part at 120
+        thresh = (y2 - y1)/(x2 - x1) * (n - x1) + y1;
+
+    elseif n < 380 % second part: horizontal
+
+        thresh = 6.237947960378145;
+
+    else % third part: decaying linear
+
+        m = -0.002007806860578; % slope
+        thresh = m * (n - 380) + 6.237947960378145; % line that starts at n = 380 (second part)
+
+    end
+
+    ys = [ys,thresh];
+end
+plot(xs,ys,'g-','LineWidth',2);
