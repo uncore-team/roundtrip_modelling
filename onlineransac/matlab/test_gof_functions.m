e339aad205d99c83cc3b9f9315603c08c7871191
Vicente Arevalo <varevalo@uma.es>
2025-04-29 20:44:50 +0200

    New version of test_gof_functions.m. Figures / datasets of the latests experiments.

--- onlineransac/matlab/test_gof_functions.m
+++ onlineransac/matlab/test_gof_functions.m
@@ -6,6 +6,7 @@ close all
 logpath = './tests';
 
 dists = { 'EXP2', 'LN3', 'LL3' };
+%dists = { 'LL3' };
 sizes = [20, 50, 150, 200, 250, 10000];
 
 nsizes = length(sizes);
@@ -13,105 +14,141 @@ ndists = length(dists);
 
 ntests = 100; % number of tests with different samples
 
-f = fopen(sprintf('%s/data.csv', logpath), 'w');
-print_header(f);
+ncombs = 2*nsizes*ndists;
+vsize = zeros(2*nsizes*ndists,1);
+vdist = cell(2*nsizes*ndists,1);
+vkmod = zeros(2*nsizes*ndists,1);
 
-tic
-status = 0;
+cont = 0;
 for s = 1:nsizes
-    sz = sizes(s);
-
-    for prevmodel = 0:1 % 0: unknown parameters / 1: known parameters
     for d = 1:ndists
-        dist = dists{d};
-
-        cont = 0;
-        while cont < ntests
+        for kmod = 0:1
             cont = cont + 1;
-
-            if strcmp(dist, 'EXP2')
-                params1 = generate_params([1e-3 76e3], [1e-5 10]);
-
-                cont2 = 0;
-                while cont2 < ntests
-                    cont2 = cont2 + 1;
-
-                    params2 = zeros(size(params1));
-                    samples = ExponentialRnd(params1(1), params1(2), 1, sz);
-    
-                    if prevmodel
-                        [reject, stat, thresh] = ExponentialGof(samples, params1(1), params1(2), 1);
-                    else
-                        [params2(1), params2(2)] = ExponentialFit(samples);
-                        % if ~ok
-                        %     print_data(f, -1, dist, prevmodel, params1, params2, -1, -1, -1);
-                        %     continue;
-                        % end
-                        [reject, stat, thresh] = ExponentialGof(samples, params2(1), params2(2), 0);
-                    end
-
-                    print_data(f, cont, cont2, dist, prevmodel, params1, params2, reject, stat, thresh);
-                end
-
-            elseif strcmp(dist, 'LN3')
-                params1 = generate_params([1e-4 76e3], [-10 10], [1e-3 8]);
-
-                cont2 = 0;
-                while cont2 < ntests
-                    cont2 = cont2 + 1;
-
-                    params2 = zeros(size(params1));
-                    samples = LognormalRnd(params1(1), params1(2), params1(3), 1, sz);
-    
-                    if prevmodel
-                        [reject, stat, thresh] = LognormalGof(samples, params1(1), params1(2), params1(3), 1);
-                    else
-                        [ok, params2(1), params2(2), params2(3)] = LognormalFit(samples);
-                        if ~ok
-                            print_data(f, -1, -1, dist, prevmodel, params1, params2, -1, -1, -1);
-                            continue;
-                        end
-                        [reject, stat, thresh] = LognormalGof(samples, params2(1), params2(2), params2(3), 0);
-                    end
-
-                    print_data(f, cont, cont2, dist, prevmodel, params1, params2, reject, stat, thresh);
-                end
-    
-            elseif strcmp(dist, 'LL3')
-                params1 = generate_params([1e-4 76e3], [1e-4 32e3], [0.05 0.4]);
-
-                cont2 = 0;
-                while cont2 < ntests
-                    cont2 = cont2 + 1;
-
-                    params2 = zeros(size(params1));
-                    samples = LoglogisticRnd(params1(1), params1(2), params1(3), 1, sz);
-    
-                    if prevmodel
-                        [reject, stat, thresh] = LoglogisticGoF(samples, params1(1), params1(2), params1(3), 1);
-                    else
-                        [params2(1), params2(2), params2(3), ko] = LoglogisticFit(samples);
-                        if ko
-                            print_data(f, -1, -1, dist, prevmodel, params1, params2, -1, -1, -1);
-                            continue;
-                        end
-                        [reject, stat, thresh] = LoglogisticGoF(samples, params2(1), params2(2), params2(3), 0);
-                    end
-
-                    print_data(f, cont, cont2, dist, prevmodel, params1, params2, reject, stat, thresh);
-                end
-            else
-                error('wrong distribution!.');
-            end
+            vsize(cont) = sizes(s);
+            vdist(cont) = dists{d};
+            vkmod(cont) = kmod;
         end
-
-        status = status + 100/nsizes/ndists/2;
-        fprintf('[%4s][sz=%5d]> %3.1f\n', dist, sz, status);
-    end
     end
 end
-toc
-fclose(f);
+
+flog = create_logfile(sprintf('%s/logfile.csv', logpath));
+print_header(flog, 'size;dist;kmodel;time');
+
+t1 = tic;
+% status = 0;
+parfor c = 1:ncombs
+    sz = vsize(c);
+    dist = vdist{c};
+    kmod = vkmod(c);
+
+    f = create_logfile(sprintf('%s/data_size=%d_dist=%s_kmodel=%d.csv', logpath, sz, dist, kmod));
+%     print_header(f, 'iter1;iter2;dist;pmodel;p1(1);p1(2);p1(3);p2(1);p2(2);p2(3);reject;stat;thresh;time');
+    print_header(f, 'iter1;iter2;p1(1);p1(2);p1(3);p2(1);p2(2);p2(3);reject;stat;thresh;time');
+
+    t2 = tic;
+    cont = 0;
+    while cont < ntests
+        cont = cont + 1;
+
+        if strcmp(dist, 'EXP2')
+            params1 = generate_params([1e-3 76e3], [1e-5 10]);
+
+            cont2 = 0;
+            cont3 = 0;
+            while (cont2 < ntests) && (cont3 < 2*ntests)
+
+                params2 = zeros(size(params1));
+                samples = ExponentialRnd(params1(1), params1(2), 1, sz);
+
+                t3 = tic;
+                cont3 = cont3 + 1;
+                if kmod
+                    [reject, stat, thresh] = ExponentialGof(samples, params1(1), params1(2), kmod);
+                else
+                    [params2(1), params2(2)] = ExponentialFit(samples);
+                    % if ~ok
+                    %     print_data(f, cont, -1, dist, knownmodel, params1, params2, -1, -1, -1);
+                    %     continue;
+                    % end
+                    [reject, stat, thresh] = ExponentialGof(samples, params2(1), params2(2), kmod);
+                end
+                t = toc(t3);
+                cont2 = cont2 + 1;
+                print_data(f, cont, cont2, dist, kmod, params1, params2, reject, stat, thresh, t);
+            end
+
+        elseif strcmp(dist, 'LN3')
+            params1 = generate_params([1e-4 76e3], [-10 10], [1e-3 8]);
+
+            cont2 = 0;
+            cont3 = 0;
+            while (cont2 < ntests) && (cont3 < 2*ntests)
+
+                params2 = zeros(size(params1));
+                samples = LognormalRnd(params1(1), params1(2), params1(3), 1, sz);
+
+                t3 = tic;
+                cont3 = cont3 + 1;
+                if kmod
+                    [reject, stat, thresh] = LognormalGof(samples, params1(1), params1(2), params1(3), kmod);
+                else
+                    [ok, params2(1), params2(2), params2(3)] = LognormalFit(samples);
+                    if ~ok
+                        t = toc(t3);
+                        print_data(f, cont, cont2, dist, kmod, params1, params2, -1, -1, -1, t);
+                        continue;
+                    end
+                    [reject, stat, thresh] = LognormalGof(samples, params2(1), params2(2), params2(3), kmod);
+                end
+                t = toc(t3);
+                cont2 = cont2 + 1;
+                print_data(f, cont, cont2, dist, kmod, params1, params2, reject, stat, thresh, t);
+
+            end
+
+        elseif strcmp(dist, 'LL3')
+            params1 = generate_params([1e-4 76e3], [1e-4 32e3], [0.05 0.4]);
+
+            cont2 = 0;
+            cont3 = 0;
+            while (cont2 < ntests) && (cont3 < 2*ntests)
+
+                params2 = zeros(size(params1));
+                samples = LoglogisticRnd(params1(1), params1(2), params1(3), 1, sz);
+
+                t3 = tic;
+                cont3 = cont3 + 1;
+                if kmod
+                    [reject, stat, thresh] = LoglogisticGoF(samples, params1(1), params1(2), params1(3), kmod);
+                else
+                    [params2(1), params2(2), params2(3), exitflag] = LoglogisticFit(samples);
+                    if exitflag < 0
+                        t = toc(t3);
+                        print_data(f, cont, cont2, dist, kmod, params1, params2, -1, -1, -1, t);
+                        continue;
+                    end
+                    [reject, stat, thresh] = LoglogisticGoF(samples, params2(1), params2(2), params2(3), kmod);
+                end
+                t = toc(t3);
+                cont2 = cont2 + 1;
+                print_data(f, cont, cont2, dist, kmod, params1, params2, reject, stat, thresh, t);
+
+            end
+        else
+            error('wrong distribution!.');
+        end
+    end
+    t=toc(t2);
+    fclose(f);
+
+    % status = status + 100/nsizes/ndists/2;
+    fprintf('[sz=%5d][dist=%4s][km=%d][time=%f]> %d/%d\n', sz, dist, kmod, t, c, ncombs);
+    print_log(flog, sz, dist, kmod, t);
+
+end
+toc(t1)
+fclose(flog);
+
 return;
 
 function num = rnd(xmin, xmax)
@@ -134,18 +171,31 @@ function params = generate_params(alim, blim, clim)
     end
 end 
 
-function print_header(f)
-    fprintf(f, 'iter1;iter2;dist;pmodel;p1(1);p1(2);p1(3);p2(1);p2(2);p2(3);reject;stat;thresh\n');
+function file = create_logfile(filename)
+    file = fopen(filename, 'w');
 end
 
-function print_data(f, iter1, iter2, dist, pmodel, params1, params2, reject, stat, thresh)
+function print_header(file, string)
+    fprintf(file, '%s\n',string);
+end
 
+function print_log(file, size, dist, knownmodel, t)
+    fprintf(file, '%d;%s;%d;%f\n', size, dist, knownmodel,t);
+end
+
+function print_data(file, iter1, iter2, dist, pmodel, params1, params2, reject, stat, thresh, t)
     if numel(params1) == 2, params1(3) = 0; end
     if numel(params2) == 2, params2(3) = 0; end
-
-    fprintf(f, '%d;%d;%s;%d;%f;%f;%f;%f;%f;%f;%f;%f;%f\n', ...
-        iter1, iter2, dist, pmodel, ...
+    % fprintf(file, '%d;%d;%s;%d;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f\n', ...
+    %     iter1, iter2, dist, pmodel, ...
+    %     params1(1), params1(2), params1(3), ...
+    %     params2(1), params2(2), params2(3), ...
+    %     reject, stat, thresh, t);
+    fprintf(file, '%d;%d;%f;%f;%f;%f;%f;%f;%f;%f;%f;%f\n', ...
+        iter1, iter2, ...
         params1(1), params1(2), params1(3), ...
         params2(1), params2(2), params2(3), ...
-        reject, stat, thresh);
+        reject, stat, thresh, t);
 end
+
+
